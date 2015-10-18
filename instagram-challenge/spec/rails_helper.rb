6bbef35e9dcf18ccdcb1791973131935acca38b2
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-10-18 10:06:59 +0100

    Factory Girl added!

--- instagram-challenge/spec/rails_helper.rb
+++ instagram-challenge/spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+require 'factory_girl_rails'
+# require_relative 'helpers/session'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
